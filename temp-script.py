daf887a9ffd81db9c125798614cb4677533c8345
Isaac Gil <isaacgil325@gmail.com>
2022-07-14 15:33:02 +0000

    job search program

---
+++ temp-script.py
@@ -0,0 +1,11 @@
+import sqlalchemy as db
+import pandas as pd
+import pprint
+
+engine = db.create_engine('sqlite:///job-search-results.db')
+engine.execute(f"CREATE TABLE jobs")
+query = engine.execute('.tables;').fetchall()
+#id_list = engine.execute(f"SELECT job_id FROM jobs WHERE user_id='joshua_feliciano';").fetchall()
+#id_list = [id[0] for id in id_list]
+#pprint.pprint(id_list)
+#print(query)
