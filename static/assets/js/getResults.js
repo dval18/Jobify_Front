7d83627339973923241437e347a66db518b1226d
Isaac Gil <isaacgil325@gmail.com>
2022-07-21 20:25:21 +0000

    db saving

--- static/assets/js/getResults.js
+++ static/assets/js/getResults.js
@@ -2,9 +2,7 @@ function getResults(job_id) {
   var title = document.getElementById("job_title_"+job_id).innerHTML;
   var c_name = document.getElementById("company_name_"+job_id).innerHTML;
   var job_location = document.getElementById("job_location_"+job_id).innerHTML;
-  var job_desciption = document.getElementById("job_description_"+job_id).innerHTML;
-  var checked = document.getElementById("job_save_"+job_id).checked;
-  console.log(checked);
+  var job_desciption = document.getElementById("job_description_"+job_id).innerHTML;;
   // console.log(title)
   // console.log(c_name)
   // console.log(job_location)
@@ -17,7 +15,6 @@ function getResults(job_id) {
        "company_name": c_name,
        "location": job_location,
        "description": job_desciption,
-       "checked":checked
     }),
     contentType: "application/json",
     dataType: 'json'
