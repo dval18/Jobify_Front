bdff572b4711f0db076f009a05ad8b8839c09aee
Isaac Gil <isaacgil325@gmail.com>
2022-07-21 17:27:00 +0000

    db integreation

--- static/assets/js/getResults.js
+++ static/assets/js/getResults.js
@@ -1,21 +1,25 @@
-function getResults() {
-  var title = document.getElementsByClassName("job_title");
-  var c_name = document.getElementsByClassName("company_name");
-  var job_location = document.getElementsByClassName("job_location");
-  var job_desciption = document.getElementsByClassName("job_description");
+function getResults(job_id) {
+  var title = document.getElementById("job_title_"+job_id).innerHTML;
+  var c_name = document.getElementById("company_name_"+job_id).innerHTML;
+  var job_location = document.getElementById("job_location_"+job_id).innerHTML;
+  var job_desciption = document.getElementById("job_description_"+job_id).innerHTML;
+  var checked = document.getElementById("job_save_"+job_id).checked;
+  console.log(checked);
+  // console.log(title)
+  // console.log(c_name)
+  // console.log(job_location)
+  // console.log(job_desciption)
+  $.ajax({
+    type: "POST",
+    url: "saved_jobs",
+    data: JSON.stringify({
+       "job_title": title,
+       "company_name": c_name,
+       "location": job_location,
+       "description": job_desciption,
+       "checked":checked
+    }),
+    contentType: "application/json",
+    dataType: 'json'
+  });
 }
-
-var server_data = [
-  {"job_title": title},
-  {"company_name": c_name},
-  {"location": job_location},
-  {"description": job_desciption},
-];
-
-$.ajax({
-  type: "POST",
-  url: "{{ url_for('save_job') }}",
-  data: JSON.stringify(server_data),
-  contentType: "application/json",
-  dataType: 'json' 
-});
